c291bba7ea3d474077356aa8083dfa307fc3dc82
Buddi <bhuvandb36@gmail.com>
2023-11-04 14:01:20 +0530

    resize camera stream window

--- image_processing/rpi_cam_stream.py
+++ image_processing/rpi_cam_stream.py
@@ -17,8 +17,14 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
 	image = frame.array
+	
+	#cv2.WINDOW_NORMAL makes the output window resizealbe
+	cv2.namedWindow('Camera Stream', cv2.WINDOW_NORMAL)
+	#resize the window according to the screen resolution
+	cv2.resizeWindow('Camera Stream', 800, 600)
+	
 	# show the frame
-	cv2.imshow("Frame", image)
+	cv2.imshow("Camera Stream", image)
 	key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
