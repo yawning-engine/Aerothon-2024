54c032c712a9fdc56b91b5474b5a7fc056f68e08
bhargav_natekar <bhargavnatekar123@gmail.com>
2023-11-13 19:05:42 +0530

    angle before sort

--- auto nav/simulation/run.py
+++ auto nav/simulation/run.py
@@ -7,6 +7,9 @@ import time
 # filed 2-
 # dronekit-sitl copter --home=13.394700,77.731850,0,180
 
+# national clg -
+# dronekit-sitl copter --home=12.950189431098176, 77.5729052662308,0,0
+
 parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
 parser.add_argument('--connect',
                         help='''Vehicle connection target string. If not specified, 
@@ -28,10 +31,13 @@ field_1_boundary_points = [(13.394727, 77.7311024), (13.393622, 77.7309197),
 
 field_2_boundary_points = [(13.394622, 77.7316004), (13.393506, 77.7314371),
                            (13.393385, 77.7318823), (13.39454, 77.7320772)]
-bms_points= [(12.941147574684594, 77.56620096320266),(12.941035238094432, 77.56620366070055),
-             (12.941024743662918, 77.56635390367906),(12.941158056317148, 77.56634852137806)]
+bms_points= [(12.941209902836812, 77.5659708787648),(12.941037373375236, 77.56592259900712),
+             (12.94096417902213, 77.56628737939842),(12.9411497789471, 77.56631688369477)]
 
-arr=drone.grid_navigation(field_1_boundary_points,15)
+national_clg=[(12.9502104499548, 77.57262628627873),(12.949732036664333, 77.57259143004251)
+              ,(12.949729422671844, 77.57288513190167),(12.950210396827655, 77.57289183742355)]
+
+arr=drone.grid_navigation(bms_points,10)
 
 #print(arr)
 
@@ -39,7 +45,7 @@ vehicle.airspeed=2
 vehicle.groundspeed=2
 
 file =  open("drone_log.txt","a") 
-#drone.arm_and_takeoff(vehicle,30)
+drone.arm_and_takeoff(vehicle,15)
 file.write("Take off location , pos="+str(drone.get_gps_location(vehicle,0,0,0)))
 
 #drone.condition_yaw()
@@ -47,7 +53,7 @@ print("relatve to abs :"+str(drone.get_gps_location(vehicle,5,5,15)))
 #print(arr)
 for i in arr:
     print("going to", drone.LocationGlobalRelative(i[0],i[1],15))
-    drone.goto_wp(vehicle, drone.LocationGlobalRelative(i[0],i[1],15),ground_speed=5)
+    drone.goto_wp(vehicle, drone.LocationGlobalRelative(i[0],i[1],15),ground_speed=2)
 
 
 
