7b12465ea2d05e09bff6d255f9da631514991508
Buddi <bhuvandb36@gmail.com>
2023-11-15 16:16:04 +0530

    smol chaanges

--- image_processing/circle_extraction.py
+++ image_processing/circle_extraction.py
@@ -78,11 +78,11 @@ template_target1 = cv2.imread(template_path_target1, cv2.IMREAD_GRAYSCALE)
 template_target2 = cv2.imread(template_path_target2, cv2.IMREAD_GRAYSCALE)
 
 template_path_hotspot1 = 'hotspot_real_smol.png'  # Change this to your hotspot image's path
-template_path_hotspot2 = 'hotspot_real_full.png'  # Change this to your hotspot image's path
+template_path_hotspot2 = 'hotspot_real_full3.png'  # Change this to your hotspot image's path
 template_hotspot1 = cv2.imread(template_path_hotspot1, cv2.IMREAD_GRAYSCALE)
 template_hotspot2 = cv2.imread(template_path_hotspot2, cv2.IMREAD_GRAYSCALE)
 
-frame = cv2.imread('drone_shot10.15.jpg', 1)
+frame = cv2.imread('drone_shot20_3.jpg', 1)
 height = int(frame.shape[0])
 width = int(frame.shape[1])
 print("Shape of you?... naah image bruh:",width, height)
