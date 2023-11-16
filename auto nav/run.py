931b2e6d582fc129ab6898780c3e80c166e772bc
bhargav_natekar <bhargavnatekar123@gmail.com>
2023-11-16 21:56:33 +0530

    test3_localpoints

--- auto nav/run.py
+++ auto nav/run.py
@@ -4,9 +4,9 @@ import time
 
 file =  open("drone_log.txt","a") 
 
-flight_alt =30
-target_alt = 20
-hotspot_alt = 10
+flight_alt = 10
+target_alt = 5
+hotspot_alt = 5
 # field 1-
 # dronekit-sitl copter --home=13.394622,77.731250,0,180
 
@@ -23,14 +23,18 @@ field_1_boundary_points = [(13.394727, 77.7311024), (13.393622, 77.7309197),
 field_2_boundary_points = [(13.394622, 77.7316004), (13.393506, 77.7314371),
                            (13.393385, 77.7318823), (13.39454, 77.7320772)]
 
-bms_points= [(12.941209902836812, 77.5659708787648),(12.941037373375236, 77.56592259900712),
-             (12.94096417902213, 77.56628737939842),(12.9411497789471, 77.56631688369477)]
+bms_points_drift= [(12.941157663780162, 77.5659762145219),(12.941008696467064, 77.5659359665737),
+             (12.940969487945662, 77.56628468686313),(12.941126330217395, 77.56629538256388)]
+
+bms_points = [(12.941196901925672, 77.5659762285262),(12.941006073867879, 77.565949406435968),
+              (12.940977344282619, 77.56625517151875),(12.941144600546245, 77.56628202178035)]
 
 national_clg=[(12.950192443726669, 77.57267429129706),(12.949852625101393, 77.5726796557151)
               ,(12.949857824812549, 77.57303375038539),(12.950228983297505, 77.57300697670405)]
 
+custom_locs = [(12.941105460631404, 77.56614384175194)]
 
-arr=drone.grid_navigation(national_clg,15)
+# arr=drone.grid_navigation(custom_locs,6)
 
 vehicle = drone.connect_real_drone()
 
@@ -38,17 +42,18 @@ vehicle = drone.connect_real_drone()
 vehicle.airspeed=0.5
 vehicle.groundspeed=0.5
 
-drone.arm_and_takeoff(vehicle,15)
+drone.arm_and_takeoff(vehicle,10)
 file.write("Take off location , pos="+str(drone.get_gps_location(vehicle,0,0,0))+"\n")
 
 detected_array = []
 target_detected = False
 
-for i in arr:
+for i in custom_locs:
     grid_point_loc = drone.LocationGlobalRelative(i[0],i[1],flight_alt)
     print("going to", grid_point_loc)
     drone.goto_wp(vehicle, grid_point_loc, ground_speed=2)
     time.sleep(2)
+    
     try:
         poi = detect(vehicle, file)
         file.write("detected things -"+str(poi)+"\n")
@@ -111,6 +116,7 @@ for i in arr:
         print("error occured during detection-"+str(e))
         file.write("error occured during detection-"+str(e)+"\n")
         continue
+    
 
 drone.RTL(vehicle)
 
